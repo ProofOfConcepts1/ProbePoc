fee890238060dc7e42ace0b838953f49ba90c5f0
Kiruthiga.5785 <Kiruthiga.5785@BA161.mnw.local>
2023-08-30 17:27:26 +0530

    commit

---
+++ BSE/src/main/java/demoBSE/demo.java
@@ -0,0 +1,35 @@
+package demoBSE;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;;
+
+public class demo {
+	
+		
+	
+		
+		
+	
+	public static void main(String[] args) throws IOException{
+		
+//		utility util = new utility();
+//		
+//		String dummyDownloadLoc = "C:\\Users\\Kiruthiga.5785\\Documents\\Blinkit\\BSE\\BseDownload\\Pledge.csv";
+//		
+//		List<String[]> allData = util.readAllDataAtOnce(dummyDownloadLoc);
+//		System.out.println(allData.size());
+//		Path path = Paths.get(dummyDownloadLoc);
+//		System.out.println(Files.lines(path).count());
+
+//		String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date());
+//		System.out.println(timeStamp);
+		
+//		 String str = "Margin Pledge/Margin Trading Funding  for trading on the Stock Exchange";
+//		 String trim = str.replaceAll("\\s{2,}"," ");
+//		 System.out.println(trim);
+		
+		String userDirectory = System.getProperty("user.dir");
+        System.out.println(userDirectory);
+        
+	}
+}
