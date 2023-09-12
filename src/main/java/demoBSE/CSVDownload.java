09acd931d2a7f8a9006ff8e60b0a07b731be21c2
Kiruthiga.5785 <azentiolearning@gmail.com>
2023-09-12 16:35:25 +0530

    commit

--- src/main/java/demoBSE/CSVDownload.java
+++ src/main/java/demoBSE/CSVDownload.java
@@ -30,70 +30,89 @@ public class CSVDownload {
 		try {
 			configFileReader = new ConfigFileReader();
 
+// Setting up the variable names         
+			
 			WebDriver driver;
 			String downloadLoc = configFileReader.getdownloadLoc();
-//    	String dummyDownloadLoc = "C:\\Users\\Kiruthiga.5785\\Documents\\Blinkit\\BSE\\BseDownload\\Pledge.csv";
+			
 
 			String url = configFileReader.getDBConnection();
 			String userName = configFileReader.getUserName();
 			String password = configFileReader.getPassword();
+			
+			int countExcel;
+			String countSqlquery;
+			int countSQL;
+			int internalCounter;
 
-			String NameoftheTargetCompany = null;
-			String ISIN = null;
-			String NamesOfthePromoters = null;
-			String PHinComapny_noOfShares = null;
-			String PHinCompany_perOfequityShare = null;
-			String PHinEncumbered_noOfShares = null;
-			String PHinEncumbered_perOfequityShare = null;
-			String DEPE_typesOfEvent = null;
-			String DEPE_dateOfCreation = null;
-			String DEPE_noOfShares = null;
-			String DEPE_perOfequityShare = null;
-			String DEPE_ReasonEncumbrance = null;
-			String DEPE_NameodEntity = null;
-			String DEPE_NameofUltimateLender = null;
-			String PECS_noOfShares = null;
-			String PECS_perOfequityShare = null;
-			String ExchangeBroadcastDT = null;
-			String Edate = null;
+			String name_of_the_target_company = null;
+			String isin_number = null;
+			String name_of_the_promoters = null;
+			String ph_in_company_no_of_shares = null;
+			String ph_in_company_per_of_equityshare = null;
+			String ph_in_encumbered_no_of_shares = null;
+			String ph_in_encumbered_per_of_equityshare = null;
+			String depe_types_of_event = null;
+			String depe_date_of_creation = null;
+			String depe_no_of_shares = null;
+			String depe_per_of_equityshare = null;
+			String depe_reason_encumbrance = null;
+			String depe_name_of_entity = null;
+			String depe_name_of_ultimatelender = null;
+			String pecs_no_of_shares = null;
+			String pecs_per_of_equityShare = null;
+			String exchange_broadcast_dt = null;
+			String scraped_on = null;
 
-//    	utility util = new utility();
+//    	Launching the chrome driver
 			driver = util.driverLaunch(downloadLoc);
-
 			driver.manage().window().maximize();
+			
+//		Launching the url of the website
 			driver.get(configFileReader.getWebsiteUrl()); // URL of the website
 
-			// Locate and click the download link or button
-			util.waitHelperClick(driver,configFileReader.getXpath(),30 );
+//      Locate and click the download link or button
+			util.waitHelperClick(driver, configFileReader.getXpath(), 30);
 			WebElement downloadLink = driver.findElement(By.xpath(configFileReader.getXpath()));
-//            downloadLink.click();
 
 			JavascriptExecutor js = (JavascriptExecutor) driver;
 			js.executeScript("arguments[0].click();", downloadLink);
 
-			// Wait for the file to download (you may need to adjust the wait time)
-			try {
-				Thread.sleep(50000); // Wait for 5 seconds
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+//      Wait for the file to download (you may need to adjust the wait time)
+
+			while (!util.rename1(downloadLoc)) {
+
+				try {
+					Thread.sleep(7000); // Wait for 5 seconds
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
 			}
 
+//		Quitting the driver after the file is downloaded
+			
 			driver.quit();
+			
+//		Renaming the downloaded file in the respective folder
 
 			String renamedloc = util.rename(downloadLoc);
 
+//		Reading the data in the downloaded csv file
+			
 			List<String[]> allData = util.readAllDataAtOnce(renamedloc);
+			
+//		Setting up the database connection and running the query to get the datas in the database
 
 			Connection con = util.dbConnection(url, userName, password);
-//    	con.setAutoCommit(True);
 
-			int countExcel = allData.size();
+			countExcel = allData.size();
 
-			String countSqlquery = "select COUNT(*) from app_pledge_api";
+			countSqlquery = "select COUNT(*) from bse_pledge";
 
 			Statement createStatement = con.createStatement();
 			ResultSet executeQuery = createStatement.executeQuery(countSqlquery);
-			int countSQL = 0;
+			countSQL = 0;
 
 			executeQuery.next();
 
@@ -101,17 +120,24 @@ public class CSVDownload {
 
 			System.out.println("Excel Count: " + countExcel + "\n" + "SQL COUNT: " + countSQL);
 
-			 differenceInCount = countExcel - countSQL;
-			 if(countExcel<countSQL)
-			 {
-			 GmailEmailSender.emailSent(configFileReader.getSubject()+'\r'+ util.getCurrentDate() + "",
+			differenceInCount = countExcel - countSQL;
+			
+//		Sending exception mail when the no of data in the source is less than the data in the database			
+			
+			if (countExcel < countSQL) {
+				GmailEmailSender.emailSent(configFileReader.getSubject() + '\r' + util.getCurrentDate() + "",
 						configFileReader.getMailbody1() + '\n' + '\n' + configFileReader.getMailbody2() + '\n' + '\n'
-								+ configFileReader.getMailbody3() + '\n' + '\n' + "There is count mismatch issue SQL count is "+countSQL+" Excel count is "+countExcel+" Hence deviation is "+differenceInCount+ '\n' + '\n'+'\n'+'\n'
+								+ configFileReader.getMailbody3() + '\n' + '\n'
+								+ "There is count mismatch issue SQL count is " + countSQL + " Excel count is "
+								+ countExcel + " Hence deviation is " + differenceInCount + '\n' + '\n' + '\n' + '\n'
 								+ configFileReader.getMailbody4());
-				
-			 }
+
+			}
+			
 			System.out.println(differenceInCount);
-			int internalCounter = 0;
+			internalCounter = 0;
+			
+//      Reading the data from the source file
 
 			if (differenceInCount != 0) {
 				System.out.println("loop started");
@@ -119,61 +145,63 @@ public class CSVDownload {
 
 					String[] row = allData.get(i);
 
-					NameoftheTargetCompany = row[0];
-					NameoftheTargetCompany = NameoftheTargetCompany.replace(".","");
-					ISIN = row[1];
-					NamesOfthePromoters = row[2];
-					PHinComapny_noOfShares = row[3];
-					PHinCompany_perOfequityShare = row[4];
-					PHinEncumbered_noOfShares = row[5];
-					PHinEncumbered_perOfequityShare = row[6];
-					DEPE_typesOfEvent = row[7];
-					DEPE_dateOfCreation = row[8];
-					DEPE_noOfShares = row[9];
-					DEPE_perOfequityShare = row[10];
-					DEPE_ReasonEncumbrance = row[11];
-					DEPE_ReasonEncumbrance = DEPE_ReasonEncumbrance.replaceAll("\\s{2,}"," ");
-					DEPE_NameodEntity = row[12];
-					DEPE_NameofUltimateLender = row[13];
-					PECS_noOfShares = row[14];
-					PECS_perOfequityShare = row[15];
+					name_of_the_target_company = row[0];
+					name_of_the_target_company = name_of_the_target_company.replace(".", "");
+					isin_number = row[1];
+					name_of_the_promoters = row[2];
+					ph_in_company_no_of_shares = row[3];
+					ph_in_company_per_of_equityshare = row[4];
+					ph_in_encumbered_no_of_shares = row[5];
+					ph_in_encumbered_per_of_equityshare = row[6];
+					depe_types_of_event = row[7];
+					depe_date_of_creation = row[8];
+					depe_no_of_shares = row[9];
+					depe_per_of_equityshare = row[10];
+					depe_reason_encumbrance = row[11];
+					depe_reason_encumbrance = depe_reason_encumbrance.replaceAll("\\s{2,}", " ");
+					depe_name_of_entity = row[12];
+					depe_name_of_ultimatelender = row[13];
+					pecs_no_of_shares = row[14];
+					pecs_per_of_equityShare = row[15];
 					try {
-						ExchangeBroadcastDT = row[16];
-						String str = "9/1/2023 9:38:55 AM"; //length = 19
-						if (ExchangeBroadcastDT.length()==str.length()) {
-						}
-						else {
-							ExchangeBroadcastDT = null;
+						exchange_broadcast_dt = row[16];
+						String str = "9/1/2023 9:38:55 AM"; // length = 19
+						if (exchange_broadcast_dt.length() == str.length()) {
+						} else {
+							exchange_broadcast_dt = null;
 						}
 					} catch (Exception e) {
 
 					}
-					Edate = util.timeStamp();
+					
+//		 Writing the data into the database
+					
+					scraped_on = util.timeStamp();
 
-					String sql = "INSERT INTO app_pledge_api "
-							+ "(NameoftheTargetCompany,ISIN,NamesOfthePromoters,PHinComapny_noOfShares,PHinCompany_perOfequityShare,PHinEncumbered_noOfShares,PHinEncumbered_perOfequityShare,DEPE_typesOfEvent,DEPE_dateOfCreation,DEPE_noOfShares,DEPE_perOfequityShare,DEPE_ReasonEncumbrance,DEPE_NameodEntity,DEPE_NameofUltimateLender,PECS_noOfShares,PECS_perOfequityShare,ExchangeBroadcastDT,Edate) "
+					String sql = "INSERT INTO bse_pledge "
+							+ "(name_of_the_target_company,isin_number,name_of_the_promoters,ph_in_company_no_of_shares,ph_in_company_per_of_equityshare,ph_in_encumbered_no_of_shares,ph_in_encumbered_per_of_equityshare,depe_types_of_event,depe_date_of_creation,depe_no_of_shares,depe_per_of_equityshare,depe_reason_encumbrance,depe_name_of_entity,depe_name_of_ultimatelender,pecs_no_of_shares,pecs_per_of_equityShare,exchange_broadcast_dt,scraped_on) "
 							+ "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
 					PreparedStatement ps = con.prepareStatement(sql);
 
-					ps.setString(1, NameoftheTargetCompany);
-					ps.setString(2, ISIN);
-					ps.setString(3, NamesOfthePromoters);
-					ps.setString(4, PHinComapny_noOfShares);
-					ps.setString(5, PHinCompany_perOfequityShare);
-					ps.setString(6, PHinEncumbered_noOfShares);
-					ps.setString(7, PHinEncumbered_perOfequityShare);
-					ps.setString(8, DEPE_typesOfEvent);
-					ps.setString(9, DEPE_dateOfCreation);
-					ps.setString(10, DEPE_noOfShares);
-					ps.setString(11, DEPE_perOfequityShare);
-					ps.setString(12, DEPE_ReasonEncumbrance);
-					ps.setString(13, DEPE_NameodEntity);
-					ps.setString(14, DEPE_NameofUltimateLender);
-					ps.setString(15, PECS_noOfShares);
-					ps.setString(16, PECS_perOfequityShare);
-					ps.setString(17, ExchangeBroadcastDT);
-					ps.setString(18, Edate);
+					ps.setString(1, name_of_the_target_company);
+					ps.setString(2, isin_number);
+					ps.setString(3, name_of_the_promoters);
+					ps.setString(4, ph_in_company_no_of_shares);
+					ps.setString(5, ph_in_company_per_of_equityshare);
+					ps.setString(6, ph_in_encumbered_no_of_shares);
+					ps.setString(7, ph_in_encumbered_per_of_equityshare);
+					ps.setString(8, depe_types_of_event);
+					ps.setString(9, depe_date_of_creation);
+					ps.setString(10, depe_no_of_shares);
+					ps.setString(11, depe_per_of_equityshare);
+					ps.setString(12, depe_reason_encumbrance);
+					ps.setString(13, depe_name_of_entity);
+					ps.setString(14, depe_name_of_ultimatelender);
+					ps.setString(15, pecs_no_of_shares);
+					ps.setString(16, pecs_per_of_equityShare);
+					ps.setString(17, exchange_broadcast_dt);
+					ps.setString(18, scraped_on);
 //    					ps.setTime(17, default);
 //    					ps.setTimestamp(17,new Timestamp());
 
@@ -195,9 +223,11 @@ public class CSVDownload {
 			}
 		} catch (Exception e) {
 			
-			GmailEmailSender.emailSent(configFileReader.getSubject()+'\r'+ util.getCurrentDate() + "",
+//			Sending exception mail for all kind of exceptions
+
+			GmailEmailSender.emailSent(configFileReader.getSubject() + '\r' + util.getCurrentDate() + "",
 					configFileReader.getMailbody1() + '\n' + '\n' + configFileReader.getMailbody2() + '\n' + '\n'
-							+ configFileReader.getMailbody3() + '\n' + '\n' + e.getMessage() + '\n' + '\n'+'\n'+'\n'
+							+ configFileReader.getMailbody3() + '\n' + '\n' + e.getMessage() + '\n' + '\n' + '\n' + '\n'
 							+ configFileReader.getMailbody4());
 			e.printStackTrace();
 
--- src/main/java/demoBSE/utility.java
+++ src/main/java/demoBSE/utility.java
@@ -1,17 +1,5 @@
 package demoBSE;
 
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-import com.opencsv.exceptions.CsvException;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -26,110 +14,155 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
+
 public class utility {
 
-    public WebDriver driverLaunch(String downloadLoc) {
-//		./driver/C:\\Users\\stagadmin\\Documents\\BSEjar\\drivers\\
-        System.setProperty("webdriver.chrome.driver",
-                "./driver/chromedriver.exe");
+	public WebDriver driverLaunch(String downloadLoc) {
 
-        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-        chromePrefs.put("download.default_directory", downloadLoc);
-        ChromeOptions options = new ChromeOptions();
-        String[] s = System.getProperty("user.dir").split("\\\\");
-        String x = s[0] + "\\" + s[1] + "\\" + s[2] + "\\";
-        options.setBinary(x + "Desktop\\chrome-win64\\chrome-win64\\chrome.exe");
-        options.setExperimentalOption("prefs", chromePrefs);
-        options.addArguments("--remote-allow-origins=*");
-//        options.addArguments("--headless");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--disable-gpu");
-        options.addArguments("–disable-extensions");
-        ChromeDriver driver = new ChromeDriver(options);
-        return driver;
-    }
+//		Browser Launch
 
-    public java.sql.Date getCurrentDate() {
-        java.util.Date today = new java.util.Date();
-        return new java.sql.Date(today.getTime());
-    }
+		// ./driver/C:\\Users\\stagadmin\\Documents\\BSEjar\\drivers\\
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+		chromePrefs.put("download.default_directory", downloadLoc);
+		ChromeOptions options = new ChromeOptions();
+		String[] s = System.getProperty("user.dir").split("\\\\");
+		String x = s[0] + "\\" + s[1] + "\\" + s[2] + "\\";
+		options.setBinary(x + "Desktop\\chrome-win64\\chrome-win64\\chrome.exe");
+		options.setExperimentalOption("prefs", chromePrefs);
+		options.addArguments("--remote-allow-origins=*");
+		// options.addArguments("--headless");
+		options.addArguments("--window-size=1920,1080");
+		options.addArguments("--disable-gpu");
+		options.addArguments("–disable-extensions");
+		ChromeDriver driver = new ChromeDriver(options);
+		return driver;
+	}
 
-    public String currentTime() {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss");
-        LocalDateTime now = LocalDateTime.now();
-        String currentTime = dtf.format(now);
-        return currentTime;
-    }
+//	To get current date
 
-    public String rename(String downloadLoc) {
+	public java.sql.Date getCurrentDate() {
+		java.util.Date today = new java.util.Date();
+		return new java.sql.Date(today.getTime());
+	}
 
-        String renameLoc = null;
-        File file = new File(downloadLoc);
-        File[] listFiles = file.listFiles();
-        for (File file2 : listFiles) {
-            if (file2.toString().contains("Pledge")) {
-                String newFileName = downloadLoc + "\\Pled_" + currentTime() + ".csv";
-                System.out.println(newFileName);
-                boolean isRenamed = file2.renameTo(new File(newFileName));
-                System.out.println(isRenamed);
-                renameLoc = newFileName;
-            }
-        }
+//	To get current time
 
-        return renameLoc;
-    }
+	public String currentTime() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss");
+		LocalDateTime now = LocalDateTime.now();
+		String currentTime = dtf.format(now);
+		return currentTime;
+	}
 
-    public Connection dbConnection(String url, String userName, String password) {
+//	To rename the downloaded file
 
-        Connection con = null;
-        try {
-            con = DriverManager.getConnection(url, userName, password);
-            System.out.println("DB connection established");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+	public String rename(String downloadLoc) {
 
-        return con;
-    }
+		String renameLoc = null;
+		File file = new File(downloadLoc);
+		File[] listFiles = file.listFiles();
+		for (File file2 : listFiles) {
+			if (file2.toString().contains("Pledge")) {
+				String newFileName = downloadLoc + "\\Pled_" + currentTime() + ".csv";
+				System.out.println(newFileName);
+				boolean isRenamed = file2.renameTo(new File(newFileName));
+				System.out.println(isRenamed);
+				renameLoc = newFileName;
+			}
+		}
 
-    public String scripCode(String str) {
-        String[] split = str.split(Pattern.quote("("));
-        str = split[split.length - 1];
-        String newStr = str.replace(")", " ");
-        System.out.println(newStr);
+		return renameLoc;
+	}
+	
+	
+	public boolean rename1(String downloadLoc) {
 
-        return newStr;
-    }
+		String renameLoc = null;
+		File file = new File(downloadLoc);
+		File[] listFiles = file.listFiles();
+		for (File file2 : listFiles) {
+			if (file2.toString().contains("Pledge")) {
+				return true;
+			}
+		}
+		return false;
 
-    public List<String[]> readAllDataAtOnce(String file) throws IOException, CsvException {
-        // Create an object of file reader
-        // class with CSV file as a parameter.
-        FileReader filereader = new FileReader(file);
+		
+	}
+	
 
-        // create csvReader object and skip first Line
-        CSVReader csvReader = new CSVReaderBuilder(filereader).withSkipLines(1).build();
-        List<String[]> allData = csvReader.readAll();
-//		System.out.println(allData.size());
-//		System.out.println(allData);
+//	Database connection
 
-        return allData;
+	public Connection dbConnection(String url, String userName, String password) {
 
-    }
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url, userName, password);
+			System.out.println("DB connection established");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-    public String timeStamp() {
-        String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date());
-//		System.out.println(timeStamp);
-        return timeStamp;
+		return con;
+	}
 
-    }
+//	To split scripcode
 
-    public void waitHelperClick(WebDriver driver, String xpath, int waitTime) {
+	public String scripCode(String str) {
+		String[] split = str.split(Pattern.quote("("));
+		str = split[split.length - 1];
+		String newStr = str.replace(")", " ");
+		System.out.println(newStr);
 
-        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(waitTime))
-                .pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+		return newStr;
+	}
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
-    }
+//	To read the data in CSV file
 
+	public List<String[]> readAllDataAtOnce(String file) throws IOException, CsvException {
+//	 Create an object of file reader
+		// class with CSV file as a parameter.
+		FileReader filereader = new FileReader(file);
+//	 create csvReader object and skip first Line
+		CSVReader csvReader = new CSVReaderBuilder(filereader).withSkipLines(1).build();
+		List<String[]> allData = csvReader.readAll();
+		// System.out.println(allData.size());
+		// System.out.println(allData);
+
+		return allData;
+
+	}
+
+//	Timestamp for Edate
+
+	public String timeStamp() {
+		String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date());
+		// System.out.println(timeStamp);
+		return timeStamp;
+
+	}
+	
+//	waithelper to click
+
+	public void waitHelperClick(WebDriver driver, String xpath, int waitTime) {
+
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(waitTime))
+				.pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
+	}
 
 }
